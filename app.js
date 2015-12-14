ec0cacdd8fbfbb204254493c7ec67dd99e768872
Adam Zerner <azerner3@gmail.com>
2015-12-14 16:43:20 -0500

    Everything working except checking to see if its a valid jump

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ angular
 
 function MainController() {
   var vm = this;
+  var jumpInProgress = false;
+  var ballPosition = [9, 7];
 
-  initializeBoard(vm);
+  initializeBoard();
 
   vm.turn = 'Eks';
   vm.changeTurn = function() {
@@ -19,26 +21,76 @@ function MainController() {
   };
 
   vm.makeMove = function(cell, row, column) {
+    if (!jumpInProgress) {
+      firstClick.apply(this, arguments);
+    }
+    else if (jumpInProgress) {
+      jump.apply(this, arguments);
+    }
+  };
+
+  vm.reset = function() {
+    jumpInProgress = false;
+    ballPosition = [9, 7];
+    vm.turn = 'Eks';
+    initializeBoard();
+  };
+
+  function firstClick(cell, row, column) {
     if (!cell) {
       vm.board[row][column] = 'player';
+      vm.changeTurn();
     }
     else if (cell === 'ball') {
-
+      jumpInProgress = true;
     }
     else {
       console.error('Click an empty space or the ball to move.');
+    }
+  };
+
+  function jump(cell, row, column) {
+    if (cell === 'player' || cell === 'ball') {
+      console.error("Can't jump to a populated space.");
       return;
     }
-    vm.changeTurn();
-  };
-}
 
-function initializeBoard(vm) {
-  vm.board = new Array(19);
+    if (!validJump(row, column)) {
+      console.error("Can't jump to that space.");
+      return;
+    }
 
-  for (var i = 0; i < 19; i++) {
-    vm.board[i] = new Array(15);
+    // remove jumped pieces
+
+    // move the ball
+    vm.board[ballPosition[0]][ballPosition[1]] = undefined;
+    vm.board[row][column] = 'ball';
+    ballPosition = [row, column];
+
+    // check if they won
+    if (vm.turn === 'Eks' && row === 0) {
+      alert('Eks wins!');
+    }
+    else if (vm.turn === 'Ohs' && row === 18) {
+      alert('Ohs wins!');
+    }
+
+    jumpInProgress = false;
   }
 
-  vm.board[9][7] = 'ball';
+  function validJump(endRow, endColumn) {
+    // I was just getting up to graph traversal in my data structures studies :(
+    // This is bound to be imperfect
+    return true;
+  }
+
+  function initializeBoard() {
+    vm.board = new Array(19);
+
+    for (var i = 0; i < 19; i++) {
+      vm.board[i] = new Array(15);
+    }
+
+    vm.board[9][7] = 'ball';
+  }
 }
